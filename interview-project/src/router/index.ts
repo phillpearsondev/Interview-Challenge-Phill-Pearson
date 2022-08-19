80fc174b5612b6a0878e16715c08408673c4aeda
Phillip A Pearson <phillip.pearson@live.co.uk>
2022-08-19 11:25:45 +0100

    clean up vue starter lelemments some more. Random number generator function works, and displays the result to the page as a string

--- interview-project/src/router/index.ts
+++ interview-project/src/router/index.ts
@@ -15,19 +15,39 @@ const router = createRouter({
 
 export default router
 
-window.onload = (event) => {
+function randomNumberGenerator() {
   // Grab the element to menipulate, then Generate a number between 0 and 100
-  var number = document.getElementById('number-test');
+  var numberElement = document.getElementById('number-test');
   var getRandomNumber = Math.floor(Math.random() * 100);
+  var numberAsString = getRandomNumber.toString();
+  var numberZero = '0';
 
-  if (number != null) {
+  // Checks if the number is equal to 100, if so then reduce the number by 1
+  // This is to keep semantically to a double-digit number
+  if (getRandomNumber === 100) {
+    getRandomNumber = 99;
+  }
+
+  // Checks if number is less than 10 then preprends '0' to the numberAsString
+  // Ensures a double-digit number if less than 10 e.g. 01, 02, 03 etc
+  if (getRandomNumber < 10) {
+    numberAsString = numberZero + numberAsString;
+  }
+
+  if (numberElement != null) {
     // Set the HTML element content as the generated number
-    number.innerHTML += getRandomNumber;
+    numberElement.innerHTML += numberAsString;
 
     // Exeption for if the element is null
   } else {
     console.log('number is null')
-  };
+  }
+
+}
+
+window.onload = (event) => {
+
+  randomNumberGenerator()
 
   console.log('page is fully loaded');
 
