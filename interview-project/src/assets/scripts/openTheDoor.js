a587acbdd4cc1c2b766f6d93a5abf132ac23b205
Phillip A Pearson <phillip.pearson@live.co.uk>
2022-08-22 11:19:53 +0100

    why does it not work?!

--- interview-project/src/assets/scripts/openTheDoor.js
+++ interview-project/src/assets/scripts/openTheDoor.js
@@ -1,3 +1,6 @@
+import sendOpen from '@/assets/scripts/websocket'
+import establishWebSocket from '@/assets/scripts/websocket'
+
 // Open the door
 
 // The function that is called once the button is pressed
@@ -9,9 +12,10 @@ export function setClickEventToButton() {
     // grab the button element from the DOM
     var PushButton = document.getElementById('push-button');
 
+
     // Checks if the element is null, if not null, add event listener
     if (PushButton != null) {
-        PushButton.addEventListener("click", openTheDoor);
+        PushButton.addEventListener("click", sendOpen);
 
     }
     // if null, throw an error
--- interview-project/src/assets/scripts/websocket.js
+++ interview-project/src/assets/scripts/websocket.js
@@ -1,16 +1,42 @@
 // Create WebSocket connection.
+export function openTheDoor() {
+    console.log('Button clicked');
+}
+
+function sendOpen() {
+    message = { opendoor: [number] }
+    socket.send(
+        alert("{opendoor: [number]}")
+    )
+}
 
 export default function establishWebSocket() {
 
-    let socket = new WebSocket("ws://" + location.host + "/")
+    var socket = new WebSocket("ws://" + location.host + "/")
+    var number = document.getElementById('number-test').val;
+
 
     if (socket != null) {
 
-        socket.onopen = function () {
-
-            socket.send("Connection Established...");
+        socket.onmessage = function (event) {
+            openTheDoor()
         }
-        console.log(socket, socket.readyState, socket.onopen);
+
+        socket.onopen = function (e) {
+
+            socket.send("Connection Established...")
+
+            // Checks if the element is null, if not null, add event listener
+            if (number != null) {
+                PushButton.addEventListener("click", sendOpen);
+
+            }
+            // if null, throw an error
+            else {
+                console.log('Push Button is null')
+            }
+        }
+        console.log(socket, socket.readyState);
 
     }
     else {
